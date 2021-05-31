ac23aff50cdf9a0b388bb4c10259b2bc11790741
G-B.GabrielBrufau <gabrielbrufau@gmail.com>
2021-05-30 21:15:39 -0300

    react

---
+++ react-basicos/src/components/Calendario/Calendario.jsx
@@ -0,0 +1,14 @@
+//react
+import React from 'react';
+//css
+import './style.css'
+
+const Calendario = ({fecha}) => {
+    return ( 
+        <>
+        <h4>{fecha}</h4>
+        </>
+     );
+}
+ 
+export default Calendario;
---
+++ react-basicos/src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+//react
+import React from 'react';
+
+function Footer({fecha}) {
+    return (
+        <>
+           <p>Todo los derechos reservados &copy;{fecha}</p>
+        </>
+    )
+}
+
+export default Footer
